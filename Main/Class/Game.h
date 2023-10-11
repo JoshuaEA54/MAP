df205b13043864393f490f161b5507c14f144c9f
JoshuaEA54 <joshua.elizondo.abarca@est.una.ac.cr>
2023-10-10 21:57:55 -0600

    font put it

--- Main/Class/Game.h
+++ Main/Class/Game.h
@@ -13,6 +13,8 @@ struct Game
 	Texture mapTex;
 	RectangleShape addRoute;
 	Event event;
+	Font font;
+	Text buttonText;
 
 	void map() {
 		
@@ -20,14 +22,19 @@ struct Game
 		window.setSize(Vector2u(1368, 690));
 
 		mapTex.loadFromFile("Images/MyMap.jpg");
-
-		 sMap = Sprite(mapTex);
+		sMap = Sprite(mapTex);
 
 		addRoute = RectangleShape(Vector2f(200, 80));
 		addRoute.setPosition(30, 660);
 		addRoute.setOutlineColor(Color::Black);
 		addRoute.setOutlineThickness(2.5);
 		addRoute.setFillColor(Color(25, 25, 110, 230));
+
+		font.loadFromFile("Images/arial_narrow_7.ttf");
+
+		buttonText = Text("Add Route", font, 28);
+		buttonText.setPosition(70, 680);
+		buttonText.setFillColor(Color::Black);
 
 		while (window.isOpen()) {
 
@@ -43,6 +50,7 @@ struct Game
 
 			window.draw(sMap);
 			window.draw(addRoute);
+			window.draw(buttonText);
 
 			window.display();
 		}
